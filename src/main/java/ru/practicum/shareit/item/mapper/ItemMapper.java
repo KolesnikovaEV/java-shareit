c1bcf52321fda662bdb1765fe9e485ed27fe4a5e
«KolesnikovaEV» <homyachokk96@yandex.ru>
2023-07-02 09:58:07 +0300

    Выполнено ТЗ, 13 спринт

--- src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
+++ src/main/java/ru/practicum/shareit/item/mapper/ItemMapper.java
@@ -1,12 +1,53 @@
 package ru.practicum.shareit.item.mapper;
 
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.item.dto.ItemDto;
 import ru.practicum.shareit.item.model.Item;
-import org.mapstruct.Mapper;
 
-@Mapper(componentModel = "spring")
-public interface ItemMapper {
-    Item toItem(ItemDto itemDto);
+import java.util.HashSet;
+import java.util.Set;
 
-    ItemDto toItemDto(Item item);
+@Component
+public class ItemMapper {
+    public ItemDto toItemDto(Item item) {
+        if (item == null) {
+            return null;
+        }
+
+        ItemDto itemDto = new ItemDto();
+
+        itemDto.setId(item.getId());
+        itemDto.setName(item.getName());
+        itemDto.setDescription(item.getDescription());
+        itemDto.setAvailable(item.getAvailable());
+        itemDto.setOwner(item.getOwner());
+        itemDto.setRequest(item.getRequest());
+        Set<Long> set = item.getReviews();
+        if (set != null) {
+            itemDto.setReviews(new HashSet<Long>(set));
+        }
+
+        return itemDto;
+    }
+
+    public static Item toItem(ItemDto itemDto) {
+        if (itemDto == null) {
+            return null;
+        }
+
+        Item item = new Item();
+
+        item.setId(itemDto.getId());
+        item.setName(itemDto.getName());
+        item.setDescription(itemDto.getDescription());
+        item.setAvailable(itemDto.getAvailable());
+        item.setOwner(itemDto.getOwner());
+        item.setRequest(itemDto.getRequest());
+        Set<Long> set = itemDto.getReviews();
+        if (set != null) {
+            item.setReviews(new HashSet<Long>(set));
+        }
+
+        return item;
+    }
 }
--- src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
+++ src/main/java/ru/practicum/shareit/user/mapper/UserMapper.java
@@ -1,13 +1,37 @@
 package ru.practicum.shareit.user.mapper;
 
-import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 import ru.practicum.shareit.user.dto.UserDto;
 import ru.practicum.shareit.user.model.User;
 
-@Mapper(componentModel = "spring")
-public interface UserMapper {
-    UserDto toUserDto(User user);
+@Component
+public class UserMapper {
+    public UserDto toUserDto(User user) {
+        if (user == null) {
+            return null;
+        }
 
-    User toUser(UserDto userDto);
+        UserDto.UserDtoBuilder userDto = UserDto.builder();
+
+        userDto.id(user.getId());
+        userDto.name(user.getName());
+        userDto.email(user.getEmail());
+
+        return userDto.build();
+    }
+
+    public User toUser(UserDto userDto) {
+        if (userDto == null) {
+            return null;
+        }
+
+        User user = new User();
+
+        user.setId(userDto.getId());
+        user.setName(userDto.getName());
+        user.setEmail(userDto.getEmail());
+
+        return user;
+    }
 
 }
