6d62e45bf1972419cc032352ccc5956639668628
«KolesnikovaEV» <homyachokk96@yandex.ru>
2023-07-31 16:07:45 +0300

    Выполнено ТЗ, 15 спринт(с исправлениями)

--- src/main/java/ru/practicum/shareit/user/dto/UserForResponseDto.java
+++ src/main/java/ru/practicum/shareit/user/dto/UserForResponseDto.java
@@ -2,13 +2,9 @@ package ru.practicum.shareit.user.dto;
 
 import lombok.Builder;
 import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
 
 @Data
 @Builder
-@Getter
-@Setter
 public class UserForResponseDto {
     private Long id;
     private String name;
